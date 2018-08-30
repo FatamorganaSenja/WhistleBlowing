3931c913a61b8809306e1e0033d7a9e9cfd156be
masirsyaad <irsyaadt@gmail.com>
2018-08-30 12:45:25 +0700

    Update: -Change UI Aplication -Add many feature and icon -And many more...

--- app/src/main/java/senja/fatamorgana/whistleblowing/VideoActivity.java
+++ app/src/main/java/senja/fatamorgana/whistleblowing/VideoActivity.java
@@ -1,13 +1,20 @@
 package senja.fatamorgana.whistleblowing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import bg.devlabs.fullscreenvideoview.FullscreenVideoView;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import senja.fatamorgana.whistleblowing.Config.SharedPrefManager;
 
 public class VideoActivity extends AppCompatActivity {
 
     FullscreenVideoView fullscreenVideoView;
+    Button btn_submit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,28 @@ public class VideoActivity extends AppCompatActivity {
                 .canSeekBackward(false)
                 .canSeekForward(false);
 
+        btn_submit = (Button)findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SweetAlertDialog pDialog = new SweetAlertDialog(VideoActivity.this);
+                pDialog.setTitleText("Anda yakin ingin dengan jawaban ini?");
+                pDialog.setConfirmText("Ya");
+                pDialog.setCancelText("Tidak");
+                pDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+                        Intent i = new Intent(VideoActivity.this, SuccessActivity.class);
+                        startActivity(i);
+                        overridePendingTransition(R.anim.fade_in_animation, R.anim.fade_out_animation);
+                        finish();
+                    }
+                });
+                pDialog.show();
+            }
+        });
     }
+
+
 }
